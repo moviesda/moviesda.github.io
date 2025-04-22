6ccfee52242e178e9d603ce215d73477450e6eab
M Jayasurya <67519756+oksurya@users.noreply.github.com>
2025-04-22 22:03:19 +0530

    sf

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="google-site-verification" content="ULWO7fwzWjXiCBXVF7iDTIQ_qTN2ifTnpWzajmU5bB8" />
+      </Head>
       <body className="antialiased ">
 
         <Main />
